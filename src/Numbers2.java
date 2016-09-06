e8afa269cac0a7be02518f93a8224ee332e205d8
Sebastian Neumann <sebastian.neumann@barstowschool.org>
2016-09-05 19:56:39 -0500

    made it working and concise

--- src/Numbers2.java
+++ src/Numbers2.java
@@ -10,13 +10,19 @@ public class Numbers2 {
 	public static void main(String[] args) {
 		while (true) {
 			Scanner sc = new Scanner(System.in);
-			ArrayList<Integer> ints = new ArrayList<Integer>(); // storage of factors found
-			System.out.print("What's your favorite number: ");
+			ArrayList<Integer> ints = new ArrayList<Integer>();
+			int i;
 			try {
-				// read input
-				int i = sc.nextInt();
+				// get input
+				System.out.print("What's your favorite number: ");
+				i = sc.nextInt();
 				if (i == 0) {
+					System.out.println("Connection Terminated");
 					break;
+				}
+				if (i < 0) {
+					System.out.println("Don't be so negative!");
+					continue;
 				}
 				
 				// calculate factors
@@ -28,17 +34,30 @@ public class Numbers2 {
 						++j;
 					}
 				}
+				if (ints.size() == 0) {
+					assert(i == 1);
+					ints.add(1);
+				}
 				
 				// print results
+				System.out.print("This is the prime factorization: ");
+				int power = 1;
 				for (int k = 0; k < ints.size(); ++k) {
-					if (k < ints.size() - 1) {
-						System.out.printf("%d x ", ints.get(k));
+					boolean lastItem = k + 1 == ints.size();
+					boolean sameFactor = !lastItem && ints.get(k) == ints.get(k + 1);
+					if (sameFactor) {
+						++power;
 					} else {
-						System.out.printf("%d\n", ints.get(k)); // end print
+						System.out.printf("%d%s", ints.get(k), (power == 1) ? "" : "^" + power);
+						power = 1;
+					}
+					if (!lastItem && !sameFactor) {
+						System.out.print(" x ");
 					}
 				}
+				System.out.print("\n");
 			} catch (InputMismatchException e) {
-				
+				System.out.println("That ain't a number.");
 			}
 		}
 	}
